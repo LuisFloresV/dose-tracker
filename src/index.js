389a78786d571f03387644e8ddde2c40c0def50a
Luis Daniel Flores Velazquez <luisfv98@outlook.com>
2021-03-27 18:07:26 -0600

    Deploy

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ const express = require('express')
 const app = new express()
 const apiDose = require('./routes/doseTraker')
 const apiUser = require('./routes/userRoutes')
-
-const { logErrors, wrapError,errorHandler } = require('./utils/errorHandler')
+const cors = require('cors')
+const { logErrors, wrapError, errorHandler } = require('./utils/errorHandler')
 const notFoundHandler = require('./utils/404Handler')
 const config = require('./config')
 
 
 // Json Parser
 app.use(express.json())
-
+app.user(cors())
 
 // routes
 apiDose(app)
