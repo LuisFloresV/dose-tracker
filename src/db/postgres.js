635b810c2a43a2119f67067418a01c6d7c64f02b
Luis Daniel Flores Velazquez <luisfv98@outlook.com>
2021-03-27 18:21:43 -0600

    Deploy

--- src/db/postgres.js
+++ src/db/postgres.js
@@ -7,6 +7,7 @@ const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_P
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: true,
 })
 
 const get = async function (table, user_id = "") {
